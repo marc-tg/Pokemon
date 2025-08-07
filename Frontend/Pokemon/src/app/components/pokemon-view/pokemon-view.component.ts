c42e2195e84d95d70dc44badb065bbabed1f6f3c
marc-tg <marctg2005@gmail.com>
2025-08-07 16:32:58 +0200

    Sprites order changed

--- Frontend/Pokemon/src/app/components/pokemon-view/pokemon-view.component.ts
+++ Frontend/Pokemon/src/app/components/pokemon-view/pokemon-view.component.ts
@@ -14,48 +14,48 @@ import { CommonModule } from '@angular/common';
 export class PokemonViewComponent implements OnInit {
   pokemon: any; // o el tipo que devuelva tu servicio
 
-activeSprite: string = '';
-spriteList: string[] = [];
-currentSpriteIndex: number = 0;
+  activeSprite: string = '';
+  spriteList: string[] = [];
+  currentSpriteIndex: number = 0;
 
 
-  constructor(private router: Router, private route: ActivatedRoute , private pokemonService: PokemonServiceService) {}
+  constructor(private router: Router, private route: ActivatedRoute, private pokemonService: PokemonServiceService) { }
+ngOnInit() {
+  this.route.queryParams.subscribe(params => {
+    const pokemonName = params['name'];
 
-  ngOnInit() {
+    if (pokemonName) {
+      this.pokemonService.getPokemonByName(pokemonName).subscribe(pokemon => {
+        this.pokemon = pokemon;
 
-    this.route.queryParams.subscribe(params => {
-      const pokemonName = params['name'];
+        // Creamos lista ordenada de sprites que queremos mostrar
+        this.spriteList = [
+          pokemon.sprites.front_default,
+          pokemon.sprites.front_shiny,
+          pokemon.sprites.back_default,
+          pokemon.sprites.back_shiny
+        ].filter(sprite => sprite !== null); // filtramos por si algún sprite es null
 
-      if (pokemonName) {
-        this.pokemonService.getPokemonByName(pokemonName).subscribe(pokemon => {
-          this.pokemon = pokemon;
-            const allSprites = Object.values(pokemon.sprites).filter(sprite => typeof sprite === 'string' && sprite !== null);
-this.spriteList = allSprites;
-  this.currentSpriteIndex = 0;
-  this.activeSprite = this.spriteList[0]; // Mostrar el primero
-
-        });
-
-        console.log("Pokemon" + this.pokemon);
-      } else {
-        this.pokemon = null;
-      }
-
-    })
-
-  }
-
-
-
-    goHome() {
-      this.router.navigate(['/']);
+        this.currentSpriteIndex = 0;
+        this.activeSprite = this.spriteList[0]; // mostramos primero front_default
+      });
+    } else {
+      this.pokemon = null;
     }
-
-changeSprites() {
-  if (this.spriteList.length > 0) {
-    this.currentSpriteIndex = (this.currentSpriteIndex + 1) % this.spriteList.length;
-    this.activeSprite = this.spriteList[this.currentSpriteIndex];
-  }
+  });
 }
+
+
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
+  changeSprites() {
+    if (this.spriteList.length > 0) {
+      this.currentSpriteIndex = (this.currentSpriteIndex + 1) % this.spriteList.length;
+      this.activeSprite = this.spriteList[this.currentSpriteIndex];
+    }
+  }
 
 }
